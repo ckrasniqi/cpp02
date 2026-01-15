ed33e14c5bf364e1d659c7882be00f5c18712851
Clirim Krasniqi <ckrasniq@3-H-3.42heilbronn.de>
2026-01-15 17:21:19 +0100

    previous ex00 has the same idea it seems

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointValue(0){
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &other){
+	std::cout << "Copy constructor called\n";
+	this->fixedPointValue = other.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &other){
+	std::cout << "Copy assignment operator called\n";
+
+	if (this != &other)
+		this->setRawBits(other.getRawBits());
+	return *this;
+}
+
+Fixed::~Fixed(){
+	std::cout << "Destructor called\n";
+}
+
+int	Fixed::getRawBits( void ) const{
+	std::cout << "getRawBits member function called\n";
+	return this->fixedPointValue;
+}
+
+void	Fixed::setRawBits( int const raw ){
+	this->fixedPointValue = raw;
+}
