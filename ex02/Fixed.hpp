37e9a88300b529d201d48776084b818f8ca6e555
Clirim Krasniqi <ckrasniq@2-B-6.42heilbronn.de>
2026-01-15 23:48:49 +0100

    Starting on ex02. Trying to do the comparison operators first

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,47 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+
+class Fixed{
+
+
+	public:
+		Fixed();
+		Fixed( const Fixed &other );
+		Fixed( const int N );
+		Fixed( const float N );
+
+		int toInt( void ) const;
+		float toFloat( void ) const;
+
+		Fixed &operator=( const Fixed &other );
+		Fixed &operator+( const Fixed &other );
+		Fixed &operator-( const Fixed &other );
+		Fixed &operator*( const Fixed &other );
+		Fixed &operator/( const Fixed &other );
+
+		bool &operator>( const Fixed &other );
+		bool &operator<( const Fixed &other );
+		bool &operator>=( const Fixed &other );
+		bool &operator<=( const Fixed &other );
+		bool &operator==( const Fixed &other );
+		bool &operator!=( const Fixed &other );
+
+		Fixed &increment++( const Fixed &other );
+
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		~Fixed();
+
+	private:
+		int fixedPointValue;
+		static const int fractionalBits = 8;
+};
+
+	std::ostream &operator<<(std::ostream &out, const Fixed &data);
+
+#endif
