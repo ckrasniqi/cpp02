a02266b902eccf59f4a12821fef1a8e8991a0a6b
Clirim Krasniqi <ckrasniq@2-B-6.42heilbronn.de>
2026-01-16 01:02:38 +0100

    small dentation fixes :D

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -94,25 +94,25 @@ Fixed	Fixed::operator--( int ){
 	return temp;
 }
 
-Fixed &Fixed::min(Fixed &a, Fixed &b){
+Fixed	&Fixed::min(Fixed &a, Fixed &b){
 	if (a < b)
 		return a;
 	return b;
 }
 
-const Fixed &Fixed::min(const Fixed &a, const Fixed &b){
+const	Fixed &Fixed::min(const Fixed &a, const Fixed &b){
 	if (a < b)
 		return a;
 	return b;
 }
 
-Fixed &Fixed::max(Fixed &a, Fixed &b){
+Fixed	&Fixed::max(Fixed &a, Fixed &b){
 	if (a > b)
 		return a;
 	return b;
 }
 
-const Fixed &Fixed::max(const Fixed &a, const Fixed &b){
+const	Fixed &Fixed::max(const Fixed &a, const Fixed &b){
 	if (a > b)
 		return a;
 	return b;
